f93e1a71fea1007bd4bc1c3714b977a3a35cb4b6
Andrew <andrewmcintyre365@gmail.com>
2020-09-17 03:28:31 -0700

    Allocates X groups of students to be assigned to X tutors.

--- assign_pr.py
+++ assign_pr.py
@@ -15,8 +15,8 @@ from examples import custom_style_1, custom_style_2
 
 from prettytable import PrettyTable
 
-
 API_URL = 'https://unr.canvaslms.com/'
+
 
 def create_logger():
     consolerHandler = logging.StreamHandler()
@@ -24,66 +24,122 @@ def create_logger():
     logger = logging.getLogger('dev')
     logger.setLevel(logging.INFO)
     logger.addHandler(consolerHandler)
-    return logger 
+    return logger
+
 
 def create_get_request(req_url, req_header, req_data={}):
     req = requests.get(req_url, headers=req_header, data=req_data)
     try:
         json_reply = json.loads(req.text)
     except ValueError:
-        logger.error(colored.red(f'Error code {req.status_code}'))# second step, get list of submissions
+        logger.error(colored.red(f'Error code {req.status_code}'))  # second step, get list of submissions
     return json_reply
+
 
 def main():
     logger = create_logger()
-    parser = argparse.ArgumentParser(description='Assigns specified students as peer reviewers for indicated assignment. Requires API token from a professor for authentication.')
-    parser.add_argument('--token', dest='API_TOKEN', metavar='token', type=str, nargs=1, default='', help='API token of professor. Can be generated from Settings on the Canvas website.', required=True)
-    parser.add_argument('--course', dest='course', metavar='course', type=int, nargs=1, default='', help='Course ID to access.', required=True)
-    #parser.add_argument('--students', dest='students', type=int, nargs='+', default='', help='Student IDs of students to assign the peer reviews to.', required=True)
+    parser = argparse.ArgumentParser(
+        description='Assigns specified students as peer reviewers for indicated assignment. Requires API token from a professor for authentication.')
+    parser.add_argument('--token', dest='API_TOKEN', metavar='token', type=str, nargs=1, default='',
+                        help='API token of professor. Can be generated from Settings on the Canvas website.',
+                        required=True)
+    # parser.add_argument('--course', dest='course', metavar='course', type=int, nargs=1, default='', help='Course ID to access.', required=True)
+    # parser.add_argument('--students', dest='students', type=int, nargs='+', default='', help='Student IDs of students to assign the peer reviews to.', required=True)
     args = parser.parse_args()
     api_token_file = args.API_TOKEN[0]
 
     # open API token text file and get the line from it
     logger.info(f'Attempting to read {api_token_file}...')
     try:
-        f = open(api_token_file, encoding='utf-16')
+        f = open(api_token_file, encoding='utf-8')
         API_TOKEN = f.readline()
         logger.info(colored.green('API token file read successfully.'))
     except IOError as e:
         logger.error(colored.red(f'I/O error {e.errno}: {e.strerror}'))
-    #except:
-        #logger.error(colored.red('Error reading API token file. Check the filename and ensure that it is correct.'))
+    # except:
+    # logger.error(colored.red('Error reading API token file. Check the filename and ensure that it is correct.'))
     AUTH_HEADER = {'Authorization': f'Bearer {API_TOKEN}'.strip('\n')}
 
-    # fetch other details
-    course_number = int(args.course[0])
-    assignment_number = 678700
-    #students = args.students
-    logger.info(f'Course ID provided: {course_number}')#Student IDs provided: {students[0]}')
+    # get course number
+    proceed = False
+    course_number = 0
+    course_name = ''
+    assignment_number = 0
+    while not proceed:
+        questions = [
+            {
+                'type': 'input',
+                'name': 'course_number',
+                'message': 'コース番号を入力：',
+            }
+        ]
+        answers = prompt(questions, style=custom_style_1)
+        course_number = answers['course_number']
+        # confirm that the assignment number is valid
+        json_reply = create_get_request(API_URL + f'api/v1/courses/{course_number}',
+                                        AUTH_HEADER)
+        try:
+            proceed = json_reply['id']
+            course_name = json_reply['name']
+            logger.info(colored.green(f"コースが見つかりました。コース名は{course_name}です。"))
+            questions = [
+                {
+                    'type': 'confirm',
+                    'message': '以上の内容で宜しいですか？',
+                    'name': 'confirm_course',
+                    'default': False,
+                }
+            ]
+            answers = prompt(questions, style=custom_style_1)
+            proceed = answers['confirm_course']
+        except KeyError:
+            logger.error(colored.red(f"エラー: {json_reply['errors'][0].get('message')}"))
+            proceed = False
 
-    # get course name
-    json_reply = create_get_request(API_URL + f'api/v1/courses/{course_number}', 
-    {'Authorization': f'Bearer {API_TOKEN}'.strip('\n')})
-    try:
-        course_name = json_reply['name']
-        logger.info(colored.green(f'Course located succesfully. Name: {course_name}'))
-    except KeyError:
-        logger.error(colored.red(f'Invalid course ID provided.'))
-        exit(1)
-
-    logger.info('\n* === Canvas Peer Review Assignment Automator === *\n')
+    # get assignment number
+    proceed = False
+    assignment_number = 0
+    while not proceed:
+        questions = [
+            {
+                'type': 'input',
+                'name': 'assignment_number',
+                'message': 'アサインメント番号を入力：',
+            }
+        ]
+        answers = prompt(questions, style=custom_style_1)
+        assignment_number = answers['assignment_number']
+        # confirm that the assignment number is valid
+        json_reply = create_get_request(API_URL + f'api/v1/courses/{course_number}/assignments/{assignment_number}',
+                                        AUTH_HEADER)
+        try:
+            proceed = json_reply['id']
+            logger.info(colored.green(f"アサインメントが見つかりました。名称は{json_reply['name']}です。"))
+            questions = [
+                {
+                    'type': 'confirm',
+                    'message': '以上の内容で宜しいですか？',
+                    'name': 'confirm_assignment',
+                    'default': False,
+                }
+            ]
+            answers = prompt(questions, style=custom_style_1)
+            proceed = answers['confirm_assignment']
+        except KeyError:
+            logger.error(colored.red(f"エラー: {json_reply['errors'][0].get('message')}"))
+            proceed = False
 
     # first, build dictionary mapping user_ids to real names
-    json_reply = create_get_request(API_URL + f'api/v1/courses/{course_number}/users', 
-    {'Authorization': f'Bearer {API_TOKEN}'.strip('\n')})
+    json_reply = create_get_request(API_URL + f'api/v1/courses/{course_number}/users',
+                                    {'Authorization': f'Bearer {API_TOKEN}'.strip('\n')})
     user_dict = {}
     for user in json_reply:
         user_dict[user['short_name']] = user['id']
 
     # next, remove teacher from list of users
-    #logger.info('リストから教師を取り出します。')
-    json_reply = create_get_request(API_URL + f'api/v1/courses/{course_number}/users', 
-    AUTH_HEADER, {'enrollment_type': 'teacher'})
+    # logger.info('リストから教師を取り出します。')
+    json_reply = create_get_request(API_URL + f'api/v1/courses/{course_number}/users',
+                                    AUTH_HEADER, {'enrollment_type': 'teacher'})
     teacher_dict = {}
     for teacher in json_reply:
         teacher_dict[teacher['short_name']] = teacher['id']
@@ -92,14 +148,14 @@ def main():
             user_dict.pop(i)
         except KeyError:
             continue
-    
+
     # display users
-    table_users = PrettyTable()
-    table_users.field_names = ['名前', 'ユーザー番号']
-    logger.info(f"Users found in course \"{course_name}\":")
+    table_students = PrettyTable()
+    table_students.field_names = ['名前', 'ユーザー番号']
+    logger.info(f"{course_name}のユーザー:")
     for i in user_dict:
-        table_users.add_row([i, user_dict[i]])
-    logger.info(table_users)
+        table_students.add_row([i, user_dict[i]])
+    logger.info(table_students)
     # user dictionary complete 
 
     # acquire user_id of people who will be peer reviewing
@@ -141,54 +197,44 @@ def main():
         proceed = answers['confirm_tutors']
 
     # create student dictionary, but remove designated tutors from list
-    student_dict = user_dict
+    student_dict = user_dict.copy()
     for i in selected_tutors:
         student_dict.pop(i)
-    
-    # get assignment number
-    proceed = False
-    assignment_number = 0
-    while proceed == False:
-        questions = [
-            {
-                'type': 'input',
-                'name': 'assignment_number',
-                'message': 'アサインメント番号を入力：',
-            }
-        ]
-        answers = prompt(questions, style=custom_style_1)
-        assignment_number = answers['assignment_number']
-        # confirm that the assignment number is valid
-        json_reply = create_get_request(API_URL + f'api/v1/courses/{course_number}/assignments/{assignment_number}', AUTH_HEADER)
-        try: 
-            proceed = json_reply['id'] 
-            logger.info(colored.green(f"アサインメントが見つかりました。名称は{json_reply['name']}です。"))
-            questions = [
-                    {
-                        'type': 'confirm',
-                        'message': '以上の内容で宜しいですか？',
-                        'name': 'confirm_assignment',
-                        'default': False,
-                    }
-                ]
-            answers = prompt(questions, style=custom_style_1)
-            proceed = answers['confirm_assignment']
-        except KeyError:
-            logger.error(colored.red(f"エラー: {json_reply['errors'][0].get('message')}"))
-            proceed = False
 
-    # show how many people the peer reviews will be alloted to each person
+    # show how many people the peer reviews will be allotted to each person
     logger.info(f'{len(selected_tutors)}人で割り当てます。')
 
+    # TESTING: adding test users
+    for i in range(0, 19):
+        student_dict[f'Test User {i}'] = i + 12345
 
-    #req_url = API_URL + f'api/v1/courses/{course_number}/assignments/{assignment_number}/submissions'
-    #req_header = {'Authorization': f'Bearer {API_TOKEN}'.strip('\n')}
-    #try:
-        #json_reply = json.loads(req.text)
-            ##logger.info(json_reply)
-            #for i in json_reply:
-                #logger.info(f"Adding submission: {i['id']}")
+    # allocate students to each student
+    # each list corresponds to a tutor
+    allocations = [[] for i in range(len(selected_tutors))]
+    number_of_students = len(student_dict)
+    for i in range(0, number_of_students):
+        student = student_dict.popitem()
+        allocations[i % len(selected_tutors)].append(student[1])
+
+    for i in range(0, len(allocations)):
+        for j in range(1, len(allocations[i])):
+            logger.info(f'POST with {allocations[i][j]} to tutor {user_dict[selected_tutors[i]]}\n')
+
+    # show users
+    table_students = PrettyTable()
+    table_students.field_names = ['名前', 'ユーザー番号']
+    logger.info(f"チューターが以下の学生の提出を担当します。")
+    for i in user_dict:
+        table_students.add_row([i, user_dict[i]])
+
+    # req_url = API_URL + f'api/v1/courses/{course_number}/assignments/{assignment_number}/submissions'
+    # req_header = {'Authorization': f'Bearer {API_TOKEN}'.strip('\n')}
+    # try:
+    # json_reply = json.loads(req.text)
+    ##logger.info(json_reply)
+    # for i in json_reply:
+    # logger.info(f"Adding submission: {i['id']}")
+
 
 if __name__ == '__main__':
     main()
-    
